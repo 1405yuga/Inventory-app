79c7b892b0671033291de9fff08e5c72a1504604
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 15:17:00 +0530

    uptodate

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -1,9 +1,18 @@
 package com.example.inventory.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+import kotlin.concurrent.Volatile
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class ItemRoomDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
 
+    companion object{
+
+    }
 }
