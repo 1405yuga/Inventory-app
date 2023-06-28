16c82dac4563799f0223c0289d805e489e8b9a6a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 15:50:20 +0530

    created viewmodel factory

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.inventory
-
-import androidx.lifecycle.ViewModel
-import com.example.inventory.data.ItemDao
-
-class InventoryViewModel(private val itemDao: ItemDao ) : ViewModel() {
-}
