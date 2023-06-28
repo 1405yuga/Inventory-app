a3fcc7e67bc3408d77f7ccbbdcca91a67400c1b0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 12:34:50 +0530

    implemented insert query

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,7 +1,12 @@
 package com.example.inventory.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertItem(item: Item)
 }
