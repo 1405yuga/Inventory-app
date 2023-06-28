ad702849d017cd0a437b7c35556a8f6dc9358550
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 15:38:22 +0530

    getting start to create viewModel

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -3,5 +3,5 @@ package com.example.inventory
 import androidx.lifecycle.ViewModel
 import com.example.inventory.data.ItemDao
 
-class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+class InventoryViewModel(private val itemDao: ItemDao ) : ViewModel() {
 }
