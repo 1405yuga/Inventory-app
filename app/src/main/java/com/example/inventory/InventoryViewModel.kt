4f68ee77923bd8005a628a1f952dc4275a826344
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 15:37:42 +0530

    intializng db object

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.ItemDao
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+}
