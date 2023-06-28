6073598882fbd9bf7627cd6144b9d7bda78183be
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 16:13:01 +0530

    implemented viewmodel instance in fragment

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -22,8 +22,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.example.inventory.databinding.FragmentAddItemBinding
+import com.example.inventory.viewmodel.InventoryViewModel
+import com.example.inventory.viewmodel.InventoryViewModelFactory
+import kotlin.reflect.KProperty
+
 
 /**
  * Fragment to add or update an item in the Inventory database.
@@ -37,6 +42,12 @@ class AddItemFragment : Fragment() {
     // when the view hierarchy is attached to the fragment
     private var _binding: FragmentAddItemBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database
+                .itemDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
