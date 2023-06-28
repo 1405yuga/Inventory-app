da3113e32daf10dea4d704edd54fe96bc2afdebb
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-28 15:28:47 +0530

    created companion object

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -4,15 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
-import kotlin.concurrent.Volatile
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class ItemRoomDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
 
     companion object{
+        @Volatile
+        private var INSTANCE : ItemRoomDatabase? = null
 
+        fun getDataBase(context: Context) : ItemRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance =
+                    Room.databaseBuilder(context, ItemRoomDatabase::class.java, "item_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
     }
 }
