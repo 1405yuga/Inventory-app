5e8ee3e7197c17f7141a49eb08d1511157964528
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-04 10:03:39 +0530

    update item implemented in viewmodel

--- app/src/main/java/com/example/inventory/viewmodel/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/viewmodel/InventoryViewModel.kt
@@ -9,70 +9,98 @@ import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
 
-class InventoryViewModel(private val itemDao: ItemDao ) : ViewModel() {
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
 
-    val allItems : LiveData<List<Item>> = itemDao.getItems().asLiveData()
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
 
     //insert item in db
-    private fun addItem(item: Item){
+    private fun addItem(item: Item) {
         viewModelScope.launch {
             itemDao.insertItem(item)
         }
     }
 
     //new item entry
-    private fun getNewItemEntry(itemName : String,itemPrice : String,itemCounts:String) : Item{
-        return Item(itemName =itemName, itemPrice = itemPrice.toDouble(), quantityInStock = itemCounts.toInt())
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCounts: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCounts.toInt()
+        )
     }
 
     //add new item in db
-    public fun addNewItem(itemName : String,itemPrice : String,itemCounts:String){
-        addItem(getNewItemEntry(itemName,itemPrice,itemCounts))
+    public fun addNewItem(itemName: String, itemPrice: String, itemCounts: String) {
+        addItem(getNewItemEntry(itemName, itemPrice, itemCounts))
     }
 
     //getting an item
-    public fun retrieveItem(id:Int) : LiveData<Item>{
+    public fun retrieveItem(id: Int): LiveData<Item> {
         return itemDao.getItem(id.toString()).asLiveData()
     }
 
     //updating values
-    public fun updateItem(item:Item){
+    public fun updateItem(item: Item) {
         viewModelScope.launch {
             itemDao.updateItem(item)
         }
     }
-    public fun sellItem(item : Item){
-        if(item.quantityInStock>0){
+
+    public fun sellItem(item: Item) {
+        if (item.quantityInStock > 0) {
             //decrease quantity by 1
             //copy function creates the same item by changing certain properties
-            val newItem = item.copy(quantityInStock = item.quantityInStock -1)
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
             updateItem(newItem)
         }
     }
 
-    fun deleteItem(item : Item){
+    fun deleteItem(item: Item) {
         viewModelScope.launch {
             itemDao.deleteItem(item)
         }
     }
 
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
 
-    fun isEntryValid(itemName : String,itemPrice: String ,itemCount:String) : Boolean{
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val updatedItem = getUpdatedItemEntry(itemId,itemName,itemPrice,itemCount)
+        updateItem(updatedItem)
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
         if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
             return false
         }
         return true
     }
 
-    fun isStockAvailable(item : Item) : Boolean{
-        return item.quantityInStock>0
+    fun isStockAvailable(item: Item): Boolean {
+        return item.quantityInStock > 0
     }
 
 }
 
 class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(InventoryViewModel::class.java)){
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return InventoryViewModel(itemDao) as T
         }
